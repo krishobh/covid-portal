23f13dd616fc59fea2ee40b9440dda3f65a0af90
krishobh <krishobhkrishna@gmail.com>
2022-06-05 16:10:41 +0530

    Code cleanup

--- src/app/models/statistics.model.ts
+++ src/app/models/statistics.model.ts
@@ -1,25 +1,31 @@
+export interface IStatisticsModel {
+    errors: string[];
+    get: string;
+    parameters: string[];
+    response: IStatisticsResponse[];
+    results: number;
+}
 export interface IStatisticsResponse {
-
-    "continent": string;
-    "country": string;
-    "population": number;
-    "cases": {
-        "new": string;
-        "active": number;
-        "critical": number | null;
-        "recovered": number;
-        "1M_pop": string;
-        "total": number;
+    continent: string;
+    country: string;
+    population: number;
+    cases: {
+        new: string;
+        active: number;
+        critical: number | null;
+        recovered: number;
+        '1M_pop': string;
+        total: number;
+    }
+    deaths: {
+        new: string;
+        '1M_pop': string;
+        total: number;
     };
-    "deaths": {
-        "new": string,
-        "1M_pop": string,
-        "total": number
+    tests: {
+        '1M_pop': string;
+        total: number;
     };
-    "tests": {
-        "1M_pop": string,
-        "total": number
-    };
-    "day": string;
-    "time": string;
+    day: string;
+    time: string;
 }
